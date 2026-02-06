0fd4b9f17b76439b8bb21581c97cda5c888e2683
Chris Masina <thato.don@gmail.com>
2026-02-06 08:57:48 -0800

    final UPDATE

--- src/main/java/za/co/rubhub/Application.java
+++ src/main/java/za/co/rubhub/Application.java
@@ -2,10 +2,17 @@ package za.co.rubhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+    
     public static void main(String[] args) {
+        // Add shutdown hook for graceful shutdown
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Shutting down gracefully...");
+        }));
+        
         SpringApplication.run(Application.class, args);
     }
 }
